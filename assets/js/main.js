9b128612b52067ae21dee55488614e8010040f3a
Raphael Andrews <raphaelandrews95@gmail.com>
2022-05-03 01:48:29 -0300

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,41 @@
+const navToggle = document.getElementById('header-toggle');
+
+function toggleMenu() {
+  const headerOverlay = document.getElementById('header-overlay');
+  headerOverlay.classList.toggle('hidden');
+}
+navToggle.addEventListener('click', toggleMenu);
+
+
+
+
+
 /*=============== SHOW MENU ===============*/
-const navToggle = document.getElementById('header-toggle'),
-      navMenu = document.getElementById('nav-menu'),
+/*const navToggle = document.getElementById('header-toggle'),
+      navMenu = document.getElementById('header-overlay'),
       navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
+/*if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.remove('hidden')
     })
-}
+}*/
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
+/*if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.add('hidden')
     })
-}
+}*/
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
+    const navMenu = document.getElementById('header-overlay')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.add('hidden')
 }
